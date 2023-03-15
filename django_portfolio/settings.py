5c5205fa434d43bf4e209291e54250c7d9f775be
Anthony Pereira <101676526+Clarensedor@users.noreply.github.com>
2023-03-15 14:30:12 -0300

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,13 +77,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
+import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': dj_database_url.config(
-         default='postgresql://postgres:postgres@localhost/postgres'
+         default='postgresql://postgres:postgres@localhost/postgres',
+         conn_max_age=600
     )
     
 
