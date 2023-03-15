11e75fa5faa2332671fe0dfe553eba54acf7c00b
Anthony Pereira <101676526+Clarensedor@users.noreply.github.com>
2023-03-15 14:49:54 -0300

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
-import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
