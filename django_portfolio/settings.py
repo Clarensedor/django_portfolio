b23c77a892241f27647f26272b3e474402c0515e
Anthony Pereira <101676526+Clarensedor@users.noreply.github.com>
2023-03-16 13:02:15 -0300

    solving, problems static files

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -142,6 +142,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
